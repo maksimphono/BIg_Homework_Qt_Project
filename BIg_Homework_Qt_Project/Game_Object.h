05ea602215dca5d7502a659c47e29808576ea6fc
Maxun <trukhinmaksimof@mail.ru>
2023-11-27 13:43:12 +0700

    placed "Game_Object" and "Game_Event" classes into a namespace to enchance encapsulation, now will overload "new" operator

--- BIg_Homework_Qt_Project/Game_Object.h
+++ BIg_Homework_Qt_Project/Game_Object.h
@@ -14,18 +14,39 @@ class Game_Event;
 
 typedef void (*EventHandler)(Game_Event& evnt);
 
-class Game_Object {
-private:
-	ull id;
-	map<EventType, vector<EventHandler>> handlers;
-public:
-	Game_Object() : id(0) {}
-};
+namespace Game_Object_NS {
+	class Game_Object;
 
-class Game_Event {
-private:
-	EventType type;
-	Game_Object target;
-	ull tickstamp;
-public:
-};
+	vector<Game_Object*> objects;
+
+	class Game_Object {
+	private:
+		
+		static ull newUniqueId() {
+			return objects.size();
+		}
+		
+		
+	private:
+		
+		map<EventType, vector<EventHandler>> handlers;
+	public:
+		ull id;
+		static Game_Object& getElementById(ull id) {
+			return *objects[id];
+		}
+		Game_Object() : id(newUniqueId()) {
+			Game_Object_NS::objects.push_back(this);
+		}
+	};
+
+	class Game_Event {
+	private:
+		EventType type;
+		Game_Object target;
+		ull tickstamp;
+	public:
+	};
+}
+
+
--- BIg_Homework_Qt_Project/main.cpp
+++ BIg_Homework_Qt_Project/main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 
-/*
-QApplication a(argc, argv);
-    MainWindow w;
-    w.setGeometry(0, 0, 600, 600);
-
-*/
 #include "Game_Object.h"
 int main(int argc, char* argv[])
 {   
+    using Game_Object_NS::Game_Object;
     Game_Object obj;
+    Game_Object obj2;
+    Game_Object_NS::objects;
+    Game_Object::getElementById(1).id = 9;
+
     return 0;
 }
+
+/*
+QApplication a(argc, argv);
+MainWindow w;
+w.setGeometry(0, 0, 600, 600);
+
+*/
